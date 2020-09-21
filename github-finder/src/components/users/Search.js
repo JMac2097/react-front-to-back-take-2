9fa0ba7261966861969bd95ed39e9fa43e55833f
John McKie <john.mckie@remarkable.net>
2020-09-21 09:35:25 +0100

    Some refactoring and setup of the clear all button

--- github-finder/src/components/users/Search.js
+++ github-finder/src/components/users/Search.js
@@ -10,7 +10,9 @@ class Search extends Component {
     };
 
     static propTypes = {
-        searchUsers: PropTypes.func.isRequired
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired
     }
 
     onSubmit = e => {
@@ -19,22 +21,38 @@ class Search extends Component {
         this.setState({ text: '' });
     }
 
-
     // pulls in the targets name value, in this case text, but could apply to email for example if that input existed
     onChange = e => this.setState({ [e.target.name] : e.target.value });
-    
-
-
 
     render() {
+        const { showClear, clearUsers } = this.props;
+
         return (
-            <div>
-                <form onSubmit={this.onSubmit} className="form">
-                    <input type="text" name="text" placeholder="Search Users" onChange={this.onChange} />
-                    <input type="submit" value="Search" className="btn btn-dark btn-block"/>
+			<div>
+				<form onSubmit={this.onSubmit} className="form">
+					<input
+						type="text"
+						name="text"
+						placeholder="Search Users"
+						onChange={this.onChange}
+					/>
+					<input
+						type="submit"
+						value="Search"
+						className="btn btn-dark btn-block"
+					/>
                 </form>
-            </div>
-        )
+                {showClear && (
+                    <button
+                        className="btn btn-light btn-block"
+                        onClick={clearUsers}
+				    >
+					    Clear
+				    </button>
+                )}
+
+			</div>
+		);
     }
 }
 
