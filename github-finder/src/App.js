26b8445dec74bf89f105278c6573dbecf9914846
John McKie <john.mckie@remarkable.net>
2020-09-22 09:24:38 +0100

    basic functionality added

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -11,10 +11,11 @@ import './App.css';
 
 class App extends Component {
 	state = {
-    users: [],
-    user: {},
+		users: [],
+		user: {},
+		repos: [],
 		loading: false,
-		alert: null,
+		alert: null
 	};
 
 	// populates the page with default list
@@ -51,6 +52,17 @@ class App extends Component {
 		this.setState({ user: res.data, loading: false });
 	};
 
+	// get users repos
+	getUserRepos = async (username) => {
+		this.setState({ loading: true });
+
+		const res = await axios.get(
+			`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_ID_GITHUB_CLIENT_SECRET}`
+		);
+
+		this.setState({ repos: res.data, loading: false });
+	};
+
 	// clear users from state
 	clearUsers = () => this.setState({ users: [], loading: false });
 
@@ -66,7 +78,7 @@ class App extends Component {
 	};
 
 	render() {
-		const { users, user, loading } = this.state;
+		const { users, user, repos, loading } = this.state;
 
 		return (
 			<Router>
@@ -95,10 +107,21 @@ class App extends Component {
 									</Fragment>
 								)}
 							/>
-              <Route exact path="/about" component={About} />
-              <Route exact path='/user/:login' render={props => (
-                <User { ...props } getUser={this.getUser} user={user} loading={loading} />
-              )} />
+							<Route exact path="/about" component={About} />
+							<Route
+								exact
+								path="/user/:login"
+								render={(props) => (
+									<User
+										{...props}
+										getUser={this.getUser}
+										getUserRepos={this.getUserRepos}
+										user={user}
+										repos={repos}
+										loading={loading}
+									/>
+								)}
+							/>
 						</Switch>
 					</div>
 				</div>
